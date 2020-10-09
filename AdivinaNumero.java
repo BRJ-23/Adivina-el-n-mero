eb5cc13c6ec5fd7873e5964e56c3a8e0894b035d
BRJ-23 <abal.varela@gmail.com>
2020-10-09 10:58:16 +0200

    Proyecto para testear git

---
+++ AdivinaNumero.java
@@ -0,0 +1,28 @@
+public class AdivinaNumero {
+     public static void main(String[] args) {
+     
+        int aleatorio = (int)(Math.random()*100);//Refundicion, le decimos que no lo convierte en entero
+		
+		Scanner entrada=new Scanner(System.in);
+		
+		int numero=0;
+		int intentos=0;
+		
+		do {
+			intentos++;
+			System.out.println("Introduce un numero, por favor");
+			numero=entrada.nextInt();
+			
+			if(aleatorio<numero) {
+				System.out.println("Más bajo");
+			}else if(aleatorio>numero) {
+				System.out.println("Más alto");
+			}
+		}while(numero!=aleatorio); 
+		System.out.println("Muy bien, has acertado el número!!!");
+		System.out.println("Lo has conseguido en "+intentos+" intentos");
+		
+		entrada.close();
+    }
+        
+}
